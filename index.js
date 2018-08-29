46762b48beaba08d3b9c70acf2001e9aaeefe1a6
Yomaira Escano <yjescano@gmail.com>
2018-08-29 01:22:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const setThisWithCall = function(fn, thisValue, arg) {
 }
 
 const setThisWithApply = function(fn, thisValue, args) {
-  return fn.apply(thisValue, [args])
+  return fn.apply(thisValue)
 }
