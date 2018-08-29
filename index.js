c16aa75c2d575b2d7357615e3ca2be58dd73a36f
Yomaira Escano <yjescano@gmail.com>
2018-08-29 01:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const justInvoke = function(fn) {
 const setThisWithCall = function(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
 }
+
+const setThisWithApply = function(fn, thisValue, args) {
+  return fn.apply(thisValue, [args])
+}
